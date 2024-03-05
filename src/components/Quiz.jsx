b34bd9b18e371a28aaeed4aa836e27c87a56fd8d
wangkodok <wangkodok@gmail.com>
2024-03-05 19:07:44 +0900

    값을 파생시키고 질문 출력 및 답변 등록

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,38 @@
 import { useState } from "react";
+import QUESTIONS from "../questions";
 
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>질문</p>;
+  const activeQuestionIndex = userAnswers.length;
+  console.log(userAnswers);
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => {
+            return (
+              <li key={answer} className="answer">
+                <button
+                  onClick={() => {
+                    handleSelectAnswer(answer);
+                  }}
+                >
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
